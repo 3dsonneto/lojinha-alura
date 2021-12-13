70937406c50f244d9125a641a50e5402186d3939
3dsonneto <edson_neto1996@hotmail.com>
2021-12-13 12:34:25 -0300

    Adicionando o text nos elementos do gridview

--- lib/widgets/grid_produtos/elemento_grid_produtos.dart
+++ lib/widgets/grid_produtos/elemento_grid_produtos.dart
@@ -1,6 +1,7 @@
 import 'package:alura_layout/models/movel.dart';
 import 'package:alura_layout/widgets/grid_produtos/degrade_elemento_grid_produtos.dart';
 import 'package:alura_layout/widgets/grid_produtos/imagem_elemento_grid_produtos.dart';
+import 'package:alura_layout/widgets/grid_produtos/titulo_elemento_grid_produtos.dart';
 import 'package:flutter/material.dart';
 
 class ElementoGridProdutos extends StatelessWidget {
@@ -11,11 +12,13 @@ class ElementoGridProdutos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
         ImagemElementoGridProdutos(
           imagem: movel.foto,
         ),
         DegradeElementoGridProdutos(),
+        TituloElementoGridProdutos(titulo: movel.titulo),
       ],
     );
   }
