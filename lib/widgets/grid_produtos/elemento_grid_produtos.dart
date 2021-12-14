8fb5723ae6d57cc5134d82b9ea184d7692cd764c
3dsonneto <edson_neto1996@hotmail.com>
2021-12-14 11:46:25 -0300

    Melhorando o design dos elementos do grid

--- lib/widgets/grid_produtos/elemento_grid_produtos.dart
+++ lib/widgets/grid_produtos/elemento_grid_produtos.dart
@@ -11,15 +11,30 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        ImagemElementoGridProdutos(
-          imagem: movel.foto,
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 2,
+            blurRadius: 8,
+            color: Colors.black12,
+          ),
+        ],
+      ),
+      margin: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ImagemElementoGridProdutos(
+              imagem: movel.foto,
+            ),
+            DegradeElementoGridProdutos(),
+            TituloElementoGridProdutos(titulo: movel.titulo),
+          ],
         ),
-        DegradeElementoGridProdutos(),
-        TituloElementoGridProdutos(titulo: movel.titulo),
-      ],
+      ),
     );
   }
 }
