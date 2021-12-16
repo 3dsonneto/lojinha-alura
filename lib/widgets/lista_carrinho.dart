d3c0b659159196c7688eac37eeb9581bcac16654
3dsonneto <edson_neto1996@hotmail.com>
2021-12-16 11:26:29 -0300

    Alinhando informações dos elementos do carrinho

--- lib/widgets/lista_carrinho.dart
+++ lib/widgets/lista_carrinho.dart
@@ -25,15 +25,52 @@ class ListaCarrinho extends StatelessWidget {
                         AssetImage("utilidades/assets/imagens/${movel.foto}"),
                     height: 92,
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(movel.titulo),
-                        Text(movel.preco.toString()),
-                        Text(item.quantidade.toString()),
-                      ],
+                  Expanded(
+                    child: Container(
+                      height: 92,
+                      margin: EdgeInsets.only(left: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(movel.titulo),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(movel.preco.toString()),
+                              Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      _aumentarQuantidade(item);
+                                    },
+                                    child: Container(
+                                      margin: EdgeInsets.all(8),
+                                      child: Icon(
+                                        Icons.add,
+                                        size: 14,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(item.quantidade.toString()),
+                                  GestureDetector(
+                                    onTap: () {
+                                      _diminuirQuantidade(item);
+                                    },
+                                    child: Container(
+                                      margin: EdgeInsets.all(8),
+                                      child: Icon(
+                                        Icons.remove,
+                                        size: 14,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
@@ -41,5 +78,13 @@ class ListaCarrinho extends StatelessWidget {
             ),
           );
         });
+  }
+
+  _aumentarQuantidade(ItemCarrinho item) {
+    item.quantidade++;
+  }
+
+  _diminuirQuantidade(ItemCarrinho item) {
+    item.quantidade--;
   }
 }
